d0326e8a2e60a7480c755aaf6a9652cf98de15f3
Brian Gammon <bgammon@gmail.com>
2016-09-18 23:54:31 -0400

    Initial commit

---
+++ app/app-module.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name rvajschat
+   * @description
+   *
+   */
+  angular
+    .module('rvajschat', [
+      'ngMaterial',
+      'ui.router',
+      'home',
+      'user'
+    ]);
+}());
---
+++ app/home/home-controller.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc object
+   * @name home.controller:HomeCtrl
+   *
+   * @description
+   *
+   */
+  angular
+    .module('home')
+    .controller('HomeCtrl', HomeCtrl);
+
+  function HomeCtrl() {
+
+  }
+}());
